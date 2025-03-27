b355f73af98e8c70ac2bbaa3c9987234fd12071a
Jagan Kumar Mohanty <jaganmohanty2001@gmail.com>
2025-03-27 21:13:55 +0530

    Pattern Program5

--- Practice Exercises/Pattern5.java
+++ Practice Exercises/Pattern5.java
@@ -1,19 +1,20 @@
 /*
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
-1 2 3 4 5
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
 */
 import java.util.Scanner;
-class pattern1{
+class Pattern5{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.print("Please enter a number: ");
-        int x = sc.nextInt();
-        for(int i=1;i<=x;i++){
-            for(int j=1;j<=5;j++){
+        int x = sc.nextInt();//5
+        for(int i=x;i>=1;i--){
+            for(int j=1;j<=i;j++){
                 System.out.print(j + " ");
+                
             }
             System.out.println();
         }
